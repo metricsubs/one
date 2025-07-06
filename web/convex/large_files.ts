1cf33cdb243e46f6d60eb3b003730cae6d341b39
dizys <dev@dizy.cc>
2025-07-06 03:22:51 -0700

    Finish S3 large_file presigned url generation implemetation

--- web/convex/large_files.ts
+++ web/convex/large_files.ts
@@ -1,20 +1,27 @@
-'use node';
-
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { AwsClient } from 'aws4fetch';
 import { v } from 'convex/values';
 import { action } from './_generated/server';
 
-const s3Client = new S3Client({
-    endpoint: process.env.S3_ENDPOINT,
+const forbiddenContentTypes = [
+    'application/x-msdownload',
+    'application/x-ms-installer',
+    'application/x-ms-shortcut',
+    'application/x-ms-application',
+    'text/html',
+    'application/xhtml+xml',
+    'application/x-shockwave-flash',
+];
+
+const maxAllowedUploadSize = 1024 * 1024 * 1024 * 10; // 10GB
+
+const s3Client = new AwsClient({
     region: process.env.S3_REGION,
-    credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
-    },
+    accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+    service: 's3',
 });
 
-export const getPresignedUploadUrl = action({
+export const getPresignedPutObjectUrl = action({
     args: {
         key: v.string(),
         contentType: v.string(),
@@ -25,15 +32,60 @@ export const getPresignedUploadUrl = action({
         if (!user) {
             throw new Error('Unauthorized');
         }
-        const command = new PutObjectCommand({
-            Bucket: process.env.S3_BUCKET,
-            Key: `${user.id}/${args.key}`,
-            ContentType: args.contentType,
-            ContentLength: args.contentLength,
+        let contentType = args.contentType;
+        if (isContentTypeForbidden(args.contentType)) {
+            contentType = 'application/octet-stream';
+        }
+        if (args.contentLength > maxAllowedUploadSize) {
+            throw new Error('File too large');
+        }
+        const url = new URL(process.env.S3_ENDPOINT!);
+        url.hostname = `${process.env.S3_BUCKET}.${url.hostname}`;
+        const fileKey = sanitizeFilename(args.key);
+        const fullFileKey = `large-files/${user.id}/${fileKey}`;
+        url.pathname = `/${fullFileKey}`;
+        const signedRequest = await s3Client.sign(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': contentType,
+                'Content-Length': args.contentLength.toString(),
+            },
         });
-        const presignedUrl = await getSignedUrl(s3Client, command, {
-            expiresIn: 60 * 15, // 15 minutes
+        return {
+            url: signedRequest.url,
+            fullFileKey,
+            contentType,
+            contentLength: args.contentLength,
+        };
+    },
+});
+
+// sanitize filename, only allow [a-z0-9-_!@#$%()], replace all other characters with underscore including spaces
+function sanitizeFilename(filename: string) {
+    return filename.replace(/[^a-z0-9-_!@#$%()]/g, '_');
+}
+
+function isContentTypeForbidden(contentType: string): boolean {
+    return forbiddenContentTypes.some((forbidden) =>
+        contentType.startsWith(forbidden)
+    );
+}
+
+export const getPresignedGetObjectUrl = action({
+    args: {
+        fullFileKey: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const user = await ctx.auth.getUserIdentity();
+        if (!user) {
+            throw new Error('Unauthorized');
+        }
+        const url = new URL(process.env.S3_ENDPOINT!);
+        url.hostname = `${process.env.S3_BUCKET}.${url.hostname}`;
+        url.pathname = `/${args.fullFileKey}`;
+        const signedRequest = await s3Client.sign(url, {
+            method: 'GET',
         });
-        return presignedUrl;
+        return signedRequest.url;
     },
 });
