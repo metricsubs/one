11cac8c39feb3fff6ddea2e2f0f340a6a6bcc8e2
dizys <dev@dizy.cc>
2025-07-05 18:55:09 -0700

    Small tweak on header

--- web/app/components/dashboard/dashboard-header.tsx
+++ web/app/components/dashboard/dashboard-header.tsx
@@ -1,4 +1,5 @@
 import { UserButton } from "@clerk/clerk-react";
+import { Link } from "react-router";
 import { cn } from "~/lib/cn";
 import LogoIcon from "../common/logo-icon";
 import { ThemeToggle } from "../common/theme-toggle";
@@ -12,10 +13,10 @@ export function DashboardHeader({ className }: DashboardHeaderProps) {
         <header className={cn("border-b sticky top-0 z-10 bg-card/90 backdrop-blur-sm", className)}>
             <div className="px-4 h-13 flex items-center justify-between">
                 <div className="flex flex-row items-center justify-between">
-                    <div className="flex items-center gap-1.5">
-                        <LogoIcon className="w-7 h-7" />
+                    <Link className="flex items-center gap-1.5 select-none" to="/">
+                        <LogoIcon className="w-6 h-6" />
                         <h1 className="text-lg font-semibold logo-font hidden sm:block">MetricSubs</h1>
-                    </div>
+                    </Link>
                 </div>
                 <div className="flex items-center gap-2">
                     <ThemeToggle />
